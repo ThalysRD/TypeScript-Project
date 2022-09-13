f6c4923b82fcd580742da6f67dc9c8da3a1c513b
ThalysRD <tgbr66@gmail.com>
2022-09-13 17:41:47 -0300

    Cria o controller para o cadastro de usuário

---
+++ src/controllers/usersController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+import userService from '../services/userService';
+
+const registeruser = async (req: Request, res: Response) => {
+  const data = req.body;
+  const result = await userService.registeruser(data);
+  return res.status(201).json({ token: result });
+};
+
+export default { registeruser };
