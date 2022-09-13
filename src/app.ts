6409f824a976d14f7aa511a0df1a2d9ed28f929d
ThalysRD <tgbr66@gmail.com>
2022-09-13 17:42:09 -0300

    Adiciona a rota no app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import productsRoute from './routes/productRoute';
+import userRoute from './routes/userRoute';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/', productsRoute);
+app.use('/', userRoute);
 
 export default app;
