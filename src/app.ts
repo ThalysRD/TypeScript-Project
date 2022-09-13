7c444d8fa8166c7645dcacaebcfa75a09f987534
ThalysRD <tgbr66@gmail.com>
2022-09-13 16:37:56 -0300

    Usa a rota para produtos

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import productsRoute from './routes/productRoute';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/', productsRoute);
 
 export default app;
