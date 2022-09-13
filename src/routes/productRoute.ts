3b702d8d6cf53cd2d18c12207db6a7ab0f943b98
ThalysRD <tgbr66@gmail.com>
2022-09-13 16:55:12 -0300

    Cria a rota para listar todos os produtos

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -4,5 +4,6 @@ import productsController from '../controllers/productsController';
 const productsRoute = Router();
 
 productsRoute.post('/products', productsController.registerProduct);
+productsRoute.get('/products', productsController.getAll);
 
 export default productsRoute;
