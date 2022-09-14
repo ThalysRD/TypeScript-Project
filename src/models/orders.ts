ad93d7588d9a9d70949187b4bfbb803b94f0f1ba
ThalysRD <tgbr66@gmail.com>
2022-09-14 16:55:22 -0300

    Adiciona tipo para a model

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -1,6 +1,7 @@
 import connection from './connection';
+import IOrders from '../interfaces/orders.interface';
 
-const getAll = async () => {
+const getAll = async () : Promise<IOrders[]> => {
   const [result] = await connection.execute(`SELECT
   O.id, O.userId, JSON_ARRAYAGG(P.id) AS productsIds
 FROM
@@ -10,7 +11,7 @@ FROM
 GROUP BY O.id
 ORDER BY O.userId
 `);
-  return result;
+  return result as IOrders[];
 };
 
 export default { getAll };
