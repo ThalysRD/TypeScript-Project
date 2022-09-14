3e3578794e56c5b2d53f8d3f6eb49c7153d0d893
ThalysRD <tgbr66@gmail.com>
2022-09-14 16:55:34 -0300

    Adiciona tipo para o service

--- src/services/ordersService.ts
+++ src/services/ordersService.ts
@@ -1,6 +1,7 @@
 import ordersModel from '../models/orders';
+import IOrders from '../interfaces/orders.interface';
 
-const getAll = async () => {
+const getAll = async () : Promise<IOrders[]> => {
   const result = await ordersModel.getAll();
   return result;
 };
