c3f49ede40abe2fffe7e4fa89175f65c1330bfa0
ThalysRD <tgbr66@gmail.com>
2022-09-14 16:48:55 -0300

    Adiciona a rota na app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import productsRoute from './routes/productRoute';
 import userRoute from './routes/userRoute';
+import ordersRoute from './routes/orderRoute';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/', productsRoute);
 app.use('/', userRoute);
+app.use('/', ordersRoute);
 
 export default app;
